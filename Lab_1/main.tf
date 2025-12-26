d615a01e73868a9a82080622b506c2d4d93630ec
stefanidmtrv <dimitrovastefani00@gmail.com>
2025-12-26 17:52:27 +0200

    Terraform resources

--- Lab_1/main.tf
+++ Lab_1/main.tf
@@ -91,29 +91,14 @@ resource "aws_internet_gateway" "internet_gateway" {
   }
 }
 
-# Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
-}
-
-# Terraform Resource Block - To Build EC2 instance in Public Subnet
-resource "aws_instance" "web_server" {
-  ami           = data.aws_ami.ubuntu.id
+resource "aws_instance" "web" {
+  ami           = "ami-099400d52583dd8c4"
   instance_type = "t3.micro"
-  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+
+  subnet_id              = "subnet-07f08773a36d7f02d"
+  vpc_security_group_ids = ["sg-0d88b1563446f3cec"]
+
   tags = {
-    Name = "Ubuntu EC2 Server"
+    "Terraform" = "true"
   }
 }
